d2e98d53fd99d3f4f95464244887f1a620d54995
Valerie Mauduit <valerie.mauduit@pricefx.com>
2023-01-03 16:47:33 +0100

    Improve the AocMap class with new constructors

--- AoC_tools/work_with_maps.py
+++ AoC_tools/work_with_maps.py
@@ -1,19 +1,46 @@
 class AocMap:
-    def __init__(self, data, position=None):
+    def __init__(self, data, position=None, origin=None):
         self.map = [[x for x in line] for line in data]
+        if origin is None:
+            self.origin = [0, 0]
+        else:
+            self.origin = origin
         if position is None:
-            self.x, self.y = 0, 0
+            self.x, self.y = self.origin[0], self.origin[1]
         else:
             self.x, self.y = position[0], position[1]
         self.width = len(data[0])
         self.height = len(data)
 
+    @classmethod
+    def empty_from_size(cls, width, height):
+        data = ['.' * width for line in range(height)]
+        return cls(data)
+
+    @classmethod
+    def from_coord(cls, coordinates, x_min=None, y_min=None, x_max=None, y_max=None):
+        if x_min is None:
+            x_min = min([coord[0] for coord in coordinates])
+        if y_min is None:
+            y_min = min([coord[1] for coord in coordinates])
+        if x_max is None:
+            x_max = max([coord[0] for coord in coordinates])
+        if y_max is None:
+            y_max = max([coord[1] for coord in coordinates])
+        map_from_coord = cls.empty_from_size(x_max - x_min + 1, y_max - y_min + 1)
+        map_from_coord.origin = [x_min, y_min]
+        map_from_coord.set_points(coordinates)
+        return map_from_coord
+
     def display(self):
         for line in self.map:
             print("".join(line))
 
-    def position(self):
+    def get_position(self):
         return [self.x, self.y]
+
+    def set_position(self, position):
+        self.x, self.y = position[0], position[1]
 
     def move(self, direction):
         displacements = {
@@ -23,25 +50,28 @@ class AocMap:
         if direction in displacements:
             self.x += displacements[direction][0]
             self.y += displacements[direction][1]
-            self.x = min([max([0, self.x]), self.width - 1])
-            self.y = min([max([0, self.y]), self.height - 1])
+            self.x = min([max([self.origin[0], self.x]), self.origin[0] + self.width - 1])
+            self.y = min([max([self.origin[0], self.y]), self.origin[1] + self.height - 1])
         else:
             raise Exception(f"The direction {direction} is not recognized by the system.")
 
-    def change_point(self, x, y, marker):
-        self.map[y][x] = marker
+    def get_point(self, position):
+        return self.map[position[1] - self.origin[1]][position[0] - self.origin[0]]
 
-    def change_points(self, coordinates, marker):
+    def set_point(self, position, marker='#'):
+        self.map[position[1] - self.origin[1]][position[0] - self.origin[0]] = marker
+
+    def set_points(self, coordinates, marker='#'):
         for coord in coordinates:
-            self.change_point(coord[0], coord[1], marker)
+            self.set_point(coord, marker)
 
-    def neighbours(self):
+    def get_neighbours(self):
         return [
-            self.map[y][x]
-            for x in range(max([0, self.x - 1]), min([self.x + 2, self.width]))
-            for y in range(max([0, self.y - 1]), min([self.y + 2, self.height]))
+            self.map[y - self.origin[1]][x - self.origin[0]]
+            for x in range(max([self.origin[0], self.x - 1]), min([self.x + 2, self.width + self.origin[0]]))
+            for y in range(max([self.origin[1], self.y - 1]), min([self.y + 2, self.height + self.origin[1]]))
             if [x, y] != [self.x, self.y]
         ]
 
     def count_neighbours(self, marker):
-        return sum([n == marker for n in self.neighbours()])
+        return sum([n == marker for n in self.get_neighbours()])
--- tests/test_tooling.py
+++ tests/test_tooling.py
@@ -21,6 +21,10 @@ def test_map():
 def test_line():
     return "20-22, 21-87"
 
+@pytest.fixture
+def test_coordinates():
+    return [[1, 5], [3, 2]]
+
 
 def test_sliding_windows(test_data):
     assert work_with_lists.sliding_windows(test_data, 2) == [[12, 42], [42, 73], [73, 1]]
@@ -37,8 +41,27 @@ def test_map_creation(test_map):
     assert test_map.y == 1
 
 
+def test_empty_map_build():
+    my_map = work_with_maps.AocMap.empty_from_size(3, 4)
+    assert my_map.map == [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
+    assert my_map.width == 3
+    assert my_map.height == 4
+
+
+def test_map_from_coord(test_coordinates):
+    without_origin = work_with_maps.AocMap.from_coord(test_coordinates)
+    assert without_origin.origin == [1, 2]
+    assert without_origin.map == [['.', '.', '#'], ['.', '.', '.'], ['.', '.', '.'], ['#', '.', '.']]
+    with_origin = work_with_maps.AocMap.from_coord(test_coordinates, x_min=0, x_max=5, y_min=0, y_max=5)
+    assert with_origin.origin == [0, 0]
+    assert with_origin.map == [
+        ['.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.'], ['.', '.', '.', '#', '.', '.'],
+        ['.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.'], ['.', '#', '.', '.', '.', '.']
+    ]
+
+
 def test_map_position(test_map):
-    assert test_map.position() == [5, 1]
+    assert test_map.get_position() == [5, 1]
 
 
 def test_map_moves(test_map):
@@ -59,30 +82,42 @@ def test_map_moves(test_map):
 
 
 def test_map_changes(test_map):
-    test_map.change_point(1, 1, '*')
+    test_map.set_point([1, 1], '*')
     assert test_map.map == [
         ['.', '.', '.', '.', '#', '.'], ['.', '*', '.', '#', '#', '.'], ['#', '.', '.', '.', '.', '.']
     ]
-    test_map.change_points([[2, 2], [5, 2], [4, 0]], 'o')
+    test_map.set_points([[2, 2], [5, 2], [4, 0]], 'o')
     assert test_map.map == [
         ['.', '.', '.', '.', 'o', '.'], ['.', '*', '.', '#', '#', '.'], ['#', '.', 'o', '.', '.', 'o']
     ]
 
 
 def test_map_neighbours(test_map):
-    neighbours = test_map.neighbours()
+    neighbours = test_map.get_neighbours()
     assert len(neighbours) == 5
     assert sum([n == '.' for n in neighbours]) == 3
     assert sum([n == '#' for n in neighbours]) == 2
     assert test_map.count_neighbours('.') == 3
     assert test_map.count_neighbours('#') == 2
     test_map.x, test_map.y = 1, 1
-    neighbours = test_map.neighbours()
+    neighbours = test_map.get_neighbours()
     assert len(neighbours) == 8
     assert sum([n == '.' for n in neighbours]) == 7
     assert sum([n == '#' for n in neighbours]) == 1
     assert test_map.count_neighbours('.') == 7
     assert test_map.count_neighbours('#') == 1
+
+
+def test_map_origin(test_map):
+    test_map.origin = [-1, -2]
+    test_map.set_position([0, 0])
+    test_map.set_point([1, 0], 'o')
+    assert test_map.map == [
+        ['.', '.', '.', '.', '#', '.'], ['.', '.', '.', '#', '#', '.'], ['#', '.', 'o', '.', '.', '.']
+    ]
+    assert test_map.count_neighbours('.') == 3
+    assert test_map.count_neighbours('#') == 1
+    assert test_map.count_neighbours('o') == 1
 
 
 def test_read_data_without_split():
