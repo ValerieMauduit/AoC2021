1870bb3c54ef96dc13484559e2e5a856d044730e
Valerie Mauduit <valerie.mauduit@pricefx.com>
2023-01-07 09:29:10 +0100

    Tooling for graphs

--- AoC_tools/work_with_graphs.py
+++ AoC_tools/work_with_graphs.py
@@ -18,10 +18,34 @@ class AocGraph:
             forbidden = self.forbidden
         else:
             forbidden = []
+        if node not in self.nodes:
+            return []
         return [neighbour for neighbour in self.neighbours[node] if neighbour not in forbidden]
 
     def forbid_node(self, node):
         self.forbidden += [node]
+
+    def distance(self, node1, node2, available_only=False):
+        if (node1 not in self.nodes) | (node2 not in self.nodes) | \
+                (available_only & ((node1 in self.forbidden) | (node2 in self.forbidden))):
+            return None
+        distances = {node1: 0}
+        not_found = True
+        all_checked = False
+        to_check = [node1]
+        while not_found & (not all_checked):
+            next_checks = []
+            for node in to_check:
+                for neighbour in self.get_neighbours(node, available_only):
+                    if neighbour not in distances:
+                        next_checks += [neighbour]
+                        distances[neighbour] = distances[node] + 1
+            not_found = (node2 not in distances)
+            all_checked = (len(distances.keys()) == len(self.nodes))
+            to_check = next_checks
+        if not_found:
+            return None
+        return distances[node2]
 
 
 def count_paths(caves, from_node, to_node, no_loop=True, forbidden=None):
@@ -37,5 +61,5 @@ def count_paths(caves, from_node, to_node, no_loop=True, forbidden=None):
                     branch_forbidden = forbidden + [from_node]
                 else:
                     branch_forbidden = forbidden
-                total += count_paths(caves, neighbour, to_node, branch_forbidden)
+                total += count_paths(caves, neighbour, to_node, no_loop, branch_forbidden)
         return total
